50c5b5c33098d2f83ec169624f19671fc71a372c
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-11-20 08:35:57 +0000

    Update version to 0.1.4

--- setup.py
+++ setup.py
@@ -1,22 +1,7 @@
 from distutils.core import setup
 
-LONGDESC = '''
-Although duck typing is generally considered the Pythonic way of dealing with
-object compatibility, it assumes that syntactic compatibility implies semantic
-compatibility.  Interfaces provide an explicit way to express semantic
-compatibility.
-
-Most existing interface modules for Python (e.g. ``abc`` and
-``zope.interface``) check that implementing classes provide all the attributes
-specified in the interface.  But they ignore the other side of the contract,
-failing to ensure that the receiver of the interface only calls operations
-specified in the interface.  The ``jute`` module checks both, ensuring that
-code works with any provider of the interface, not just the provider with
-which it was tested.
-'''
-
 GITHUB_URL = 'https://github.com/jongiddy/jute'
-VERSION = '0.1.2'
+VERSION = '0.1.4'
 
 setup(
     name='jute',
@@ -25,7 +10,6 @@ setup(
     version=VERSION,
     description='Interface module that verifies both providers and callers',
     keywords=['interface', 'polymorphism'],
-    longdesc=LONGDESC,
     author='Jonathan Patrick Giddy',
     author_email='jongiddy@gmail.com',
     url=GITHUB_URL,
