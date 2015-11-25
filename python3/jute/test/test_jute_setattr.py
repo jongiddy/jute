ef5a0ecac1477d89cb274e83b04fa969b475db52
Jonathan Giddy <jon.giddy@clusterhq.com>
2015-11-25 08:45:11 +0000

    Forgot to add test file to previous commit

---
+++ python3/jute/test/test_jute_setattr.py
@@ -0,0 +1,102 @@
+import unittest
+
+from jute import Interface, implements, underlying_object
+
+
+class ISettable(Interface):
+
+    def __setattr__(self, name, value):
+        """Set an attribute."""
+
+
+class SetAttrTestMixin:
+
+    def get_test_object(self):
+        return object()
+
+    def test_dot_name(self):
+        setdash = self.get_test_object()
+        setdash.foo = 1
+        self.assertTrue(hasattr(underlying_object(setdash), 'foo_'))
+        self.assertEqual(underlying_object(setdash).foo_, 1)
+
+    def test_setattr_function(self):
+        setdash = self.get_test_object()
+        setattr(setdash, 'foo', 1)
+        self.assertTrue(hasattr(underlying_object(setdash), 'foo_'))
+        self.assertEqual(underlying_object(setdash).foo_, 1)
+
+    def test_direct(self):
+        setdash = self.get_test_object()
+        setdash.__setattr__('foo', 1)
+        self.assertTrue(hasattr(underlying_object(setdash), 'foo_'))
+        self.assertEqual(underlying_object(setdash).foo_, 1)
+
+
+@implements(ISettable)
+class SetDash:
+
+    def __setattr__(self, name, value):
+        object.__setattr__(self, name + '_', value)
+
+
+class SetAttrInstanceTests(SetAttrTestMixin, unittest.TestCase):
+
+    def get_test_object(self):
+        return SetDash()
+
+
+class SetAttrInterfaceTests(SetAttrTestMixin, unittest.TestCase):
+
+    def get_test_object(self):
+        return ISettable(SetDash())
+
+
+# setattr() ignores dynamically generated __setattr__, so interface must
+# do the same
+
+class GeneratedTestMixin:
+
+    def get_test_object(self):
+        return object()
+
+    def test_dot_name(self):
+        setdash = self.get_test_object()
+        setdash.foo = 1
+        self.assertTrue(hasattr(underlying_object(setdash), 'foo'))
+        self.assertEqual(underlying_object(setdash).foo, 1)
+
+    def test_setattr_function(self):
+        setdash = self.get_test_object()
+        setattr(setdash, 'foo', 1)
+        self.assertTrue(hasattr(underlying_object(setdash), 'foo'))
+        self.assertEqual(underlying_object(setdash).foo, 1)
+
+    def test_direct(self):
+        setdash = self.get_test_object()
+        setdash.__setattr__('foo', 1)
+        self.assertTrue(hasattr(underlying_object(setdash), 'foo'))
+        self.assertEqual(underlying_object(setdash).foo, 1)
+
+
+@implements(ISettable)
+class GeneratedSetDash:
+
+    def __getattr__(self, name):
+        if name == '__setattr__':
+            def f(obj, name, value):
+                object.__setattr__(obj, name + '_', value)
+            return f
+        raise AttributeError(name)
+
+
+class GeneratedInstanceTests(GeneratedTestMixin, unittest.TestCase):
+
+    def get_test_object(self):
+        return GeneratedSetDash()
+
+
+class GeneratedInterfaceTests(GeneratedTestMixin, unittest.TestCase):
+
+    def get_test_object(self):
+        return ISettable(GeneratedSetDash())
